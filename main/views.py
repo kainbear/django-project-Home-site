cefc543deb2610d6bd34aa68ef68461aea60c600
Stanislav <70844672+kainbear@users.noreply.github.com>
2024-09-11 19:57:03 +0700

    ver 0.5

--- main/views.py
+++ main/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     context  = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context  = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том что магазин классный клевый с кучей товара'
+    }
+
+    return render(request, 'main/about.html', context)
