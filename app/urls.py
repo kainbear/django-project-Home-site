360c63f7a05bedf78c4e34d9446b0eb850da81a0
Stanislav <70844672+kainbear@users.noreply.github.com>
2024-09-18 17:03:00 +0700

    Fixing debuger

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
 from app.settings import DEBUG
 
 urlpatterns = [
