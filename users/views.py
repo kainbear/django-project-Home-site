7e7c21c5ebc2c21dbc686d087ecf23f398419b39
Stanislav <70844672+kainbear@users.noreply.github.com>
2024-09-26 14:55:20 +0700

    final edits

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
@@ -42,7 +44,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            
+
             session_key = request.session.session_key
 
             user = form.instance
@@ -76,12 +78,18 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
-    context = {"title": "Home - Кабинет", "form": form}
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+    context = {"title": "Home - Кабинет", "form": form, "orders": orders}
     return render(request, "users/profile.html", context)
-
-
-def users_cart(request):
-    return render(request, "users/users_cart.html")
 
 
 @login_required
