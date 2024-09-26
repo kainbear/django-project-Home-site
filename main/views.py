4e8bdd2003f8f8747fb344400189fbaf6b432e30
Stanislav <70844672+kainbear@users.noreply.github.com>
2024-09-26 17:54:54 +0700

    refactor and indexe's CatalogView ListView

--- main/views.py
+++ main/views.py
@@ -1,24 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = "main/index.html"
 
-    context  = {
-        'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME',
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - Главная"
+        context["content"] = "Магазин мебели - HOME"
+        return context
 
-    return render(request, 'main/index.html', context)
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О нас'
+        context['content'] = "О нас"
+        context['text_on_page'] = "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        return context
+
+# def index(request):
+#     context = {
+#         "title": "Home - Главная",
+#         "content": "Магазин мебели HOME",
+#     }
+#     return render(request, "main/index.html", context)
 
 
-def about(request):
-    context  = {
-        'title': 'Home - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Текст о том что магазин классный клевый с кучей товара'
-    }
-
-    return render(request, 'main/about.html', context)
+# def about(request):
+#     context = {
+#         "title": "Home - О нас",
+#         "content": "О нас",
+#         "text_on_page": "Текст о том что магазин классный клевый с кучей товара",
+#     }
+#     return render(request, "main/about.html", context)
